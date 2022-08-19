4b9da898df868aa0778ace42d01c826007419e8a
navneetco0 <navneetharsh3@gmail.com>
2022-08-19 09:16:57 +0530

    drawer dont

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,12 +8,14 @@ import {
   DrawerFooter,
   DrawerOverlay,
   DrawerContent,
-  useDisclosure
+  useDisclosure,
 } from "@chakra-ui/react";
 import { useRef } from "react";
+import { Apple } from "../Assets/SVGs/Apple";
+import { Android } from "../Assets/SVGs/Android";
 
 export const Navbar = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = useRef();
   return (
     <>
@@ -24,7 +26,7 @@ export const Navbar = () => {
         position="fixed"
         width={"100%"}
       >
-        <Box mr={"2.5%"} cursor={"pointer"} onClick={onOpen} >
+        <Box mr={"2.5%"} cursor={"pointer"} onClick={onOpen}>
           <Menu />
         </Box>
         <Box>
@@ -38,6 +40,7 @@ export const Navbar = () => {
             p="12px 16px"
             boxShadow="0px 0px 8px rgb(0 0 0 / 10%), 0px 4px 4px rgb(0 0 0 / 4%)"
             borderRadius="500px"
+            bg={"white"}
           >
             <Person whiteSpace="nowrap" /> Log in
           </Flex>
@@ -54,36 +57,93 @@ export const Navbar = () => {
         </Flex>
       </Flex>
       <Drawer
-      size={'xs'}
+        size={"xs"}
         isOpen={isOpen}
-        placement='left'
+        placement="left"
         onClose={onClose}
         finalFocusRef={btnRef}
       >
         <DrawerOverlay />
         <DrawerContent>
-
           <DrawerBody>
-            <Box bg="black" color={'white'} textAlign="center" padding={'12px 16px'} fontSize='18px' fontWeight={'500'} borderRadius={'8px'} mt="20px">Sign up</Box>
-            <Box bg="#EEEEEE" color={'Black'} textAlign="center" padding={'12px 16px'} fontSize='18px' fontWeight={'500'} borderRadius={'8px'} mt="10px">Log in</Box>
-            <Text>Create a business account</Text>
-            <Text>Add your restaurant</Text>
-            <Text>sign up to deliver</Text>
+            <Box
+              cursor={"pointer"}
+              bg="black"
+              color={"white"}
+              textAlign="center"
+              padding={"12px 16px"}
+              fontSize="18px"
+              fontWeight={"500"}
+              borderRadius={"8px"}
+              mt="20px"
+            >
+              Sign up
+            </Box>
+            <Box
+              cursor={"pointer"}
+              bg="#EEEEEE"
+              color={"Black"}
+              textAlign="center"
+              padding={"12px 16px"}
+              fontSize="18px"
+              fontWeight={"500"}
+              borderRadius={"8px"}
+              m="10px 0 25px"
+            >
+              Log in
+            </Box>
+            <Flex
+              fontSize={"14px"}
+              fontWeight="500"
+              direction={"column"}
+              h="90px"
+              color="black"
+              justifyContent="space-between"
+            >
+              <Text>Create a business account</Text>
+              <Text>Add your restaurant</Text>
+              <Text>sign up to deliver</Text>
+            </Flex>
           </DrawerBody>
           <DrawerFooter>
-            <Flex gap={'20px'}>
-                <Image w={'56px'} h="56px" src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/31ee382bd0e6ed84.svg" alt="" />
-                <Text fontSize="16px" fontWeight={'500'}>There's more to love in the app</Text>
-            </Flex>
-            <Flex>
-               <Box></Box>
-               <Box></Box>
-            </Flex>
+            <Box>
+              <Flex gap={"20px"}>
+                <Image
+                  w={"56px"}
+                  h="56px"
+                  src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/31ee382bd0e6ed84.svg"
+                  alt=""
+                />
+                <Text fontSize="16px" fontWeight={"500"}>
+                  There's more to love in the app
+                </Text>
+              </Flex>
+              <Flex fontSize={"14px"} fontWeight="500" color={"black"} gap="15px" m={"20px 0 40px"}>
+                <Flex
+                  p="12px 16px"
+                  whiteSpace={"nowrap"}
+                  boxShadow="0px 0px 8px rgb(0 0 0 / 10%), 0px 4px 4px rgb(0 0 0 / 4%)"
+                  borderRadius={"500px"}
+                  backgroundColor="#E2E2E2"
+                >
+                  <Apple />
+                  <Text>iPhone</Text>
+                </Flex>
+                <Flex
+                  p="12px 16px"
+                  whiteSpace={"nowrap"}
+                  boxShadow="0px 0px 8px rgb(0 0 0 / 10%), 0px 4px 4px rgb(0 0 0 / 4%)"
+                  borderRadius={"500px"}
+                  backgroundColor="#E2E2E2"
+                >
+                  <Android />
+                  <Text>Android</Text>
+                </Flex>
+              </Flex>
+            </Box>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
     </>
   );
 };
-
-
