b84c77f09ee4fe31a90c778676461d0c369d68ca
navneetco0 <navneetharsh3@gmail.com>
2022-08-21 10:57:05 +0530

    working on homepage

--- src/Components/HomeSearch.jsx
+++ src/Components/HomeSearch.jsx
@@ -1,19 +1,62 @@
-import { Box, Text, Flex, Button } from "@chakra-ui/react";
+import { Text, Flex, Button, Square } from "@chakra-ui/react";
+import { Bottom } from "../Assets/SVGs/Bottom";
+import { Timer } from "../Assets/SVGs/Timer";
 import { SearchDiv } from "./SearchDiv";
 
-export const HomeSearch = ()=>{
-    return (
-        <Flex direction={"column"} w={'100%'} h='100vh' backgroundColor="#f9c810" backgroundImage={"uri(https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/9b21aa66b4922ae2.png)"} justifyContent="center" p={'40px'}>
-             <Text fontSize={"52px"} fontWeight="700" m={"40px 0"}>Order food to your door</Text>
-            
-             <Flex >
-               <SearchDiv/>
-               <Button/>
-               <Button bg={"black"} color="white" fontSize="18px" fontWeight={"500"} padding="12px 16px" _hover={{
-                bg:"#333333"
-               }}> Find Food </Button>
-             </Flex>
-             <Flex><Text>Sign In</Text><Text>for your recent address</Text></Flex>
+export const HomeSearch = () => {
+  return (
+    <Flex
+      direction={"column"}
+      w={"100%"}
+      h="100vh"
+      backgroundColor="#f9c810"
+      backgroundImage={
+        "uri(https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/9b21aa66b4922ae2.png)"
+      }
+      justifyContent="center"
+      p={"40px"}
+    >
+      <Text fontSize={"52px"} fontWeight="700" m={"40px 0"}>
+        Order food to your door
+      </Text>
+
+      <Flex direction={"row"} gap="10px">
+        <SearchDiv />
+        <Flex
+          direction={"row"}
+          gap={"10px"}
+          alignItems="center"
+          backgroundColor={"white"}
+        >
+          <Square w="48px" h={"48px"} cursor="pointer">
+            <Timer />
+          </Square>
+          <Flex direction={"row"} alignItems={"center"} h="inherit" cursor={"pointer"}>
+            <Text fontSize={'16px'} fontWeight="500"> Deliver now</Text>
+            <Square w="48px" h="48px">
+              <Bottom />
+            </Square>
+          </Flex>
         </Flex>
-    )
-}
+        <Button
+          bg={"black"}
+          color="white"
+          fontSize="18px"
+          fontWeight={"500"}
+          padding="12px 16px"
+          h="inherit"
+          _hover={{
+            bg: "#333333",
+          }}
+        >
+          {" "}
+          Find Food{" "}
+        </Button>
+      </Flex>
+      <Flex fontSize="14px" fontWeight="500" gap="5px" mt="20px">
+        <Text textDecoration="underline" cursor="pointer" >Sign In </Text> {" "}
+        <Text> for your recent address</Text>
+      </Flex>
+    </Flex>
+  );
+};
--- src/Components/SearchDiv.jsx
+++ src/Components/SearchDiv.jsx
@@ -1,10 +1,12 @@
-import { Flex, Input } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import { useState } from "react";
 import {Location} from "../Assets/SVGs/Location"
 export const SearchDiv = ()=>{
+    const [focus, setFocus] = useState(false);
     return (
-        <Flex h={'56px'} w='50%' backgroundColor='white' alignItems={'center'}>
+        <Flex h={'56px'} w='50%' backgroundColor='white' alignItems={'center'} padding="8px 16px" gap="10px" boxShadow={focus?"inset 0 -2px 0 black":""}>
           <Location/>
-          <Input type={'text'} outline="transparent" border="transparent"/>
+          <input type="text" style={{border:'transparent', backgroundColor:'transparent', outline:"transparent", flexGrow:1}} placeholder="Enter delivery address" onFocus={()=>setFocus(true)} onBlur={()=>setFocus(false)}/>
         </Flex>
     )
 }
